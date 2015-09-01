74fccf7667b8fc413a16e3ac1e4a75db5381d50c
coderHBB <developer@houseofbluebeans.com>
2015-09-01 18:04:47 +0530

    First Comit

---
+++ Assets/HBB_Scripts/roomManager.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class roomManager : MonoBehaviour {
+	public float feet_Width;
+	public float inch_Width;
+	public float feet_Depth;
+	public float inch_Depth;
+
+	public GameObject vertA1;
+	public GameObject vertA2;
+	public GameObject vertB1;
+	public GameObject vertB2;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Q)) {
+			print ("meters : "+inMeters (feet_Width, inch_Width));
+			changeDimension ();
+		}
+	}
+
+	float inMeters (float inFeet,float inInches) //converting feet to meters (1 unity unit = 1 meter) to maintain size proportion
+	{
+		float totalFeet;
+		float meter;
+		totalFeet = inFeet + (inInches * 0.0833333f); // 1 inch = 0.0833333
+		meter = totalFeet * 0.3048f;
+		print ("in mm : " +inMillimeters (totalFeet));
+		return meter;
+	}
+
+	float inMillimeters (float totalFeet) //converting to millimeters,because we are getting the assets from UL in mm
+	{
+		float mm;
+		mm = totalFeet * 304.8f; // 1 foot = 304.8
+		return mm;
+	}
+
+	void changeDimension ()
+	{
+		//Width of Kitchen
+		float widthOfKitchenFromUser = inMeters (feet_Width, inch_Width);
+		float currentWidthOfKitchen = Vector3.Distance (vertA1.transform.position, vertB1.transform.position);
+		float alteredWidth = widthOfKitchenFromUser - currentWidthOfKitchen;
+
+		vertA1.transform.position = new Vector3(vertA1.transform.position.x - (alteredWidth)/2,vertA1.transform.position.y,vertA1.transform.position.z);
+		vertB1.transform.position = new Vector3(vertB1.transform.position.x + (alteredWidth)/2,vertB1.transform.position.y,vertB1.transform.position.z);
+
+		vertA2.transform.position = new Vector3(vertA2.transform.position.x - (alteredWidth)/2,vertA2.transform.position.y,vertA2.transform.position.z);
+		vertB2.transform.position = new Vector3(vertB2.transform.position.x + (alteredWidth)/2,vertB2.transform.position.y,vertB2.transform.position.z);
+
+		//Depth of Kitchen
+		float depthOfKitchenFromUser = inMeters (feet_Depth, inch_Depth);
+		float currentDepthOfKitchen = Vector3.Distance (vertA1.transform.position, vertA2.transform.position);
+		float alteredDepth = depthOfKitchenFromUser - currentDepthOfKitchen;
+
+		vertA1.transform.position = new Vector3(vertA1.transform.position.x,vertA1.transform.position.y,vertA1.transform.position.z - (alteredDepth)/2);
+		vertB1.transform.position = new Vector3(vertB1.transform.position.x,vertB1.transform.position.y,vertB1.transform.position.z - (alteredDepth)/2);
+		
+		vertA2.transform.position = new Vector3(vertA2.transform.position.x ,vertA2.transform.position.y,vertA2.transform.position.z + (alteredDepth)/2);
+		vertB2.transform.position = new Vector3(vertB2.transform.position.x,vertB2.transform.position.y,vertB2.transform.position.z + (alteredDepth)/2);
+
+	}
+}
---
+++ Assets/HBB_Scripts/wallScript.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class wallScript : MonoBehaviour {
+
+	public GameObject initialVertex;  // initialPoint of the wall
+	public GameObject finalVertex;	  // finalPoint of the wall
+	public float lengthOfWall;        // The length of the wall, from initialVertex to FinalVertex.
+
+	//to calculate angle of rotation
+	Vector3 thirdPoint;
+	float hyp;
+	float oppositeSide;
+	float angle;
+	/// 
+
+	public Vector3 boundExtends; // Vector that stores the mesh bounds of the wall
+	Vector3 wallPosition;        // position of the wall
+	Vector3 alteredPosition;      // altered position of wall after
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		if(Input.GetKeyDown (KeyCode.Space))
+			wallUpdate();
+	}
+
+	void bounds () //mesh bounds of the wall
+	{
+		boundExtends = gameObject.GetComponent<Renderer>().bounds.extents;
+	}
+
+	void wallUpdate ()
+	{
+
+		wallPosition = (initialVertex.transform.position + finalVertex.transform.position)/2; //positioning the wall
+
+		// keeps the wall edges without overlapping each other
+		if(gameObject.name == "topWall")
+			alteredPosition = new Vector3 (wallPosition.x, -0.5f, wallPosition.z + boundExtends.z);
+		if(gameObject.name == "bottomWall")
+			alteredPosition = new Vector3 (wallPosition.x, -0.5f, wallPosition.z - boundExtends.z);
+		if(gameObject.name == "leftWall")
+			alteredPosition = new Vector3 (wallPosition.x - boundExtends.x, -0.5f, wallPosition.z);
+		if(gameObject.name == "rightWall")
+			alteredPosition = new Vector3 (wallPosition.x + boundExtends.x, -0.5f, wallPosition.z);
+		///
+		gameObject.transform.position = alteredPosition; // assigning position
+
+//		gameObject.transform.position = (initialVertex.transform.position + finalVertex.transform.position)/2; //positioning the wall
+		lengthOfWall = Vector3.Distance (initialVertex.transform.position,finalVertex.transform.position); // calculates the length of the wall
+		gameObject.transform.localScale = new Vector3(lengthOfWall,2.4384f,0.1f);  // scaling the wall
+
+		//calculate angle of wall
+		thirdPoint = new Vector2 (finalVertex.transform.position.x, initialVertex.transform.position.z);
+		hyp = Vector2.Distance (new Vector3 (initialVertex.transform.position.x, initialVertex.transform.position.z), new Vector3 (finalVertex.transform.position.x, finalVertex.transform.position.z));
+		oppositeSide = Vector2.Distance (thirdPoint, new Vector3 (finalVertex.transform.position.x, finalVertex.transform.position.z));
+		if (oppositeSide != hyp)
+			angle = Mathf.Asin (oppositeSide / hyp) * 57.3f;
+		else
+			angle = 90;
+
+		if ((initialVertex.transform.position.x > finalVertex.transform.position.x && initialVertex.transform.position.z < finalVertex.transform.position.z) ||
+		    (initialVertex.transform.position.z > finalVertex.transform.position.z && initialVertex.transform.position.x < finalVertex.transform.position.x)){
+			gameObject.transform.rotation = Quaternion.Euler (0, angle, 0);
+			
+		}
+		else{
+			gameObject.transform.rotation = Quaternion.Euler (0, -angle, 0); // inverts the wall
+			
+		}
+
+		bounds ();
+	}
+}
